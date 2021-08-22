c4d5ac2bed3f62171911507d99915418243d0de7
Виксна Максим Евгеньевич <mviksna@phoenixit.ru>
2021-08-22 13:00:52 +0300

    +

---
+++ frontend/scripts/index.js
@@ -0,0 +1,26 @@
+document.querySelector(".nxt-step").onclick = () => {
+    document.querySelector(".custom-input").disabled = true;
+    document.querySelector(".block-input > div:nth-child(2) > p").style.opacity = "0";
+
+    document.querySelector(".step-1 > div:nth-child(1)").style.background = "#F6F6F6";
+    document.querySelector(".step-2 > div:nth-child(1)").style.background = "#0b75ff";
+
+    document.querySelector(".step-1 > div:nth-child(1)").style.color = "#A8ADC7";
+    document.querySelector(".step-2 > div:nth-child(1)").style.color = "white";
+
+    document.querySelector(".step-1 > div:nth-child(2) > span").style.color = "#A8ADC7";
+    document.querySelector(".step-2 > div:nth-child(2) > span").style.color = "#193660";
+
+    document.querySelector(".block-title > span").innerHTML = "Step 2/2";
+    document.querySelector(".block-title > h2").innerHTML = "Results of text analysis";
+
+    document.querySelector(".nxt-step").innerHTML = "Try Again";
+
+    document.querySelector(".similar-result").style.display = "flex";
+    document.querySelector(".result").style.display = "flex";
+
+    document.querySelector(".similar-result").style.opacity = "1";
+    document.querySelector(".result").style.opacity = "1";
+
+    // document.querySelector(".block-input").innerHTML = document.querySelector(".block-input").innerHTML + ""
+}
